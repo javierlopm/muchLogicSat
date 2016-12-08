3b37605b2bada35f9d11b6d9f37ec4d68720f4c8
Javier <javierloplom@gmail.com>
2016-12-08 18:52:18 -0400

    rep helper

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+from enum import Enum
+
+class Dir(Enum):
+    north = 1
+    east  = 2
+    south = 3
+    west  = 4
+
+class Clause(object):
+    def __init__(self, i,j,d):
+        self.i   = i
+        self.j   = j
+        self.dir = d
+
+    def unify_rep(self):
+        if (self.d == Dir.west):
+            self.i -= 1
+            self.d == Dir.east
+
+        if (self.d == Dir.south):
+            self.j -= 1
+            self.d = Dir.north
+
+    def __str__(self):
+        return "q(" + str(self.i) + "," + str(self.j) + "," + str(self.dir.name[0]) + ")"
+        
+
 def read_input():
     in_str = input()
     toks = in_str.split()
@@ -5,3 +32,4 @@ def read_input():
     c = toks[1]
     rep = toks[2:]
     return (rep,show)
+
